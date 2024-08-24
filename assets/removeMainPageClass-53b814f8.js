c443ab48bba5eda221ec2537e96a2bf7142306f4
LesiaUKR <106915140+LesiaUKR@users.noreply.github.com>
2024-08-24 05:46:35 +0000

    Deploying to gh-pages from @ LesiaUKR/wonka_world_candy_match_landing_page@d5cee2de64c85bc1810aae6ce9fab1f0fa9b3e97 🚀

--- assets/removeMainPageClass-53b814f8.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector(".js-home-page");document.location.pathname.includes("index")||e.classList.remove("active-page")})();
-//# sourceMappingURL=removeMainPageClass-53b814f8.js.map
