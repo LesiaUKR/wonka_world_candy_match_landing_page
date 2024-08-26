edbfc5fb436480fad6c3c4b65401f7a9d66994db
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-08-26 21:12:10 +0300

    feat: gallery in process...

---
+++ src/js/gallery_swiper.js
@@ -0,0 +1,23 @@
+let gallerySwiper = new Swiper('.gallery-swiper', {
+  pagination: {
+    el: '.gallery-swiper-pagination',
+  },
+
+  spaceBetween: 16,
+
+  breakpoints: {
+    1440: {
+      slidesPerView: 1,
+      spaceBetween: 110,
+
+      navigation: {
+        nextEl: '.gallery-swiper-button-prev',
+        prevEl: '.gallery-swiper-button-next',
+      },
+      pagination: {
+        el: '.gallery-swiper-pagination',
+        clickable: true,
+      },
+    },
+  },
+});
