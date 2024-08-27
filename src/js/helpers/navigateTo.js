367af0476140d40b184478a66cc647938424df98
didukhroma <didukhroma@gmail.com>
2024-08-27 11:53:41 +0300

    fix: navigate to

--- src/js/helpers/navigateTo.js
+++ src/js/helpers/navigateTo.js
@@ -5,11 +5,17 @@ export const navigateTo = hash => {
 
   const id = hash.replace('#', '');
   const elementRef = document.getElementById(id);
+  const position =
+    elementRef.getBoundingClientRect().top +
+    window.scrollY -
+    headerRef.offsetHeight;
 
   bodyScrollLock.enableBodyScroll(document.body);
+  elementRef.scrollIntoView({});
   setTimeout(() => {
     window.scroll({
-      top: elementRef.offsetTop - headerRef.offsetHeight,
+      // top: elementRef.offsetTop - headerRef.offsetHeight,
+      top: position,
       behavior: 'smooth',
     });
   });
