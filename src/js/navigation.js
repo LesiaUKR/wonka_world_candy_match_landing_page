68010a0fa8ba3d6d41ba827c63fa8f55dc634d81
didukhroma <didukhroma@gmail.com>
2024-08-28 09:57:50 +0300

    fix: added new images

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -2,14 +2,11 @@ import { navigateTo } from './helpers/navigateTo.js';
 
 (() => {
   const linksRef = document.querySelectorAll('.js-nav-link');
-  //   console.log(linksRef);
 
   const handleClick = e => {
     e.preventDefault();
     navigateTo(e.target.hash);
   };
-
-  //   linksRef.forEach(link => link.remove('click', handleClick));
 
   linksRef.forEach(link => link.addEventListener('click', handleClick));
 })();
