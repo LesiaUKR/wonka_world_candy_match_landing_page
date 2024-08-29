8a0c18f166e35223ec47c4e1fa842ea3eae4ee83
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-08-29 15:43:21 +0300

    feat: add animation for benefits and gallery

--- src/js/gallery_swiper.js
+++ src/js/gallery_swiper.js
@@ -7,7 +7,10 @@ let gallerySwiper = new Swiper('.gallery-swiper', {
   pagination: {
     el: '.gallery-swiper-pagination',
   },
-
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
   slidesPerView: 1,
   centeredSlides: true,
   spaceBetween: 16,
